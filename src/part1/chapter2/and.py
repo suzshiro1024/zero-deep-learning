5fb83a4486c5e7969b68ca7482dcc15464ba680b
suzshiro1024 <tix131423113210@gmail.com>
2023-02-14 00:23:20 +0900

    2-3-1

---
+++ src/part1/chapter2/and.py
@@ -0,0 +1,15 @@
+def and_gate(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = w1 * x1 + w2 * x2
+
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
+
+
+if __name__ == "__main__":
+    print(f"AND(0,0)= {and_gate(0,0)}")
+    print(f"AND(1,0)= {and_gate(1,0)}")
+    print(f"AND(0,1)= {and_gate(0,1)}")
+    print(f"AND(1,1)= {and_gate(1,1)}")
