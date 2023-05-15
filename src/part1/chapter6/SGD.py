dcb23877ceba707c340b817ce270312768a76608
suzshiro1024 <tix131423113210@gmail.com>
2023-05-16 00:32:33 +0900

    6-1-2

---
+++ src/part1/chapter6/SGD.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class SGD:
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.learning_rate * grads[key]
