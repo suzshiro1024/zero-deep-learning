6157acd878497078d4aaaec68d4a0ac9514865d7
suzshiro1024 <tix131423113210@gmail.com>
2023-05-18 00:02:13 +0900

    6-1-5

---
+++ src/part1/chapter6/AdaGrad.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class AdaGrad:
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= (
+                self.learning_rate * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+            )
