e87bcfc11bb2acc8a8ce4899dedbeefabb5f4895
suzshiro1024 <tix131423113210@gmail.com>
2023-06-05 00:17:08 +0900

    add common function of CNN

---
+++ src/part1/common/pooling.py
@@ -0,0 +1,52 @@
+import sys
+import os
+
+sys.path.append(os.pardir)
+
+from common.im2col import im2col
+from common.col2im import col2im
+
+import numpy as np
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=2, padding=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.padding = padding
+
+        self.x = None
+        self.arg_max = None
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.padding)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+
+        arg_max = np.argmax(col, axis=1)
+        out = np.max(col, axis=1)
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        self.x = x
+        self.arg_max = arg_max
+
+        return out
+
+    def backward(self, d_output):
+        d_output = d_output.transpose(0, 2, 3, 1)
+
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((d_output.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = d_output.flatten()
+        dmax = dmax.reshape(d_output.shape + (pool_size,))
+
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        d_x = col2im(
+            dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.padding
+        )
+
+        return d_x
